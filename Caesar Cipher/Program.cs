d8ce59630cf2837a6ca2ebcd60184595c7dd4651
Mark Olah <114992682+OlahMark19@users.noreply.github.com>
2023-10-09 18:58:34 +0300

    Add project files.

---
+++ Caesar Cipher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caesar Cipher Encryption and Decryption");
+            Console.Write("Enter a text: ");
+            string txt = Console.ReadLine();
+
+            int shift = 3;
+           
+            string encrypted = Encrypt(txt, shift);
+            string decrypted = Decrypt(txt, shift);
+
+            Console.WriteLine($"Encrypted message: {encrypted}");
+            Console.WriteLine($"Decrypted message: {decrypted}");
+
+            Console.ReadKey();
+        }
+
+        static string Encrypt(string txt, int shift)
+        {
+            char[] chars = txt.ToCharArray();
+
+            for(int i =0; i < chars.Length; i++) 
+            {
+                if (char.IsLetter(chars[i]))
+                {
+                    char offset = char.IsUpper(chars[i]) ? 'A' : 'a';
+                    chars[i] = (char)((chars[i] - offset + shift) % 26 + offset);
+                }
+            }
+
+            return new string(chars);
+        }
+        static string Decrypt(string txt, int shift)
+        {
+            return Encrypt(txt, 26 - shift); 
+        }
+        
+    }
+}
